7cfa64667f609c4ab80d7719f006b8f0c90c4cd1
maty1975 <matias.gomez.a01@mail.pucv.cl>
2025-09-09 17:53:26 -0300

    TEST 2

--- Assets/Scripts/Game/carta.cs
+++ Assets/Scripts/Game/carta.cs
@@ -10,6 +10,7 @@ public class carta : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        print("PROBANDO");
         btn_card = GetComponent<Button>();
         text_card = GetComponentInChildren<TextMeshProUGUI>();
         text_card.text = nombre_carta;
